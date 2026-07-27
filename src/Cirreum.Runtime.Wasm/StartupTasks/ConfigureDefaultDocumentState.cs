d3f7ae0a856422b553de1714e839b2a93fb10ac6
Glen Banta <glen.banta@outlook.com>
2026-07-27 15:32:29 -0700

    Updated to use the new IBrowserDocumentState in place of the removed IPageState.

---
+++ src/Cirreum.Runtime.Wasm/StartupTasks/ConfigureDefaultDocumentState.cs
@@ -0,0 +1,28 @@
+﻿namespace Cirreum.Runtime.StartupTasks;
+
+using System.Threading.Tasks;
+
+sealed class ConfigureDefaultDocumentState(
+	IBrowserDocumentState documentState,
+	IJSAppModule jsApp
+) : IStartupTask {
+
+	int IStartupTask.Order => int.MinValue;
+
+	ValueTask IStartupTask.ExecuteAsync() {
+
+		using var _ = documentState.CreateNotificationScope();
+
+		documentState.SetIsStandAlone(jsApp.IsStandAlone());
+
+		documentState.SetAppName(jsApp.GetAppName());
+
+		documentState.SetDocumentTitleSeparator("|");
+		documentState.SetDocumentTitlePrefix("");
+		documentState.SetDocumentTitleSuffix($"{documentState.AppName}");
+
+		return ValueTask.CompletedTask;
+
+	}
+
+}
